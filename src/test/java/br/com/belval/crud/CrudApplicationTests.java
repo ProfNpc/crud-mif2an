8a2d65c09d9cadb28fe3d78fcdd795750b04e7cd
Alessandro <alessandro.tacini@fieb.edu.br>
2023-08-18 22:35:30 -0300

    Versão Inicial

---
+++ src/test/java/br/com/belval/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.belval.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
