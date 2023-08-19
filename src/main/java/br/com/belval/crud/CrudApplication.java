3fd603e12b1fa447941c0ae6a8a11a9f36b3f79a
Alessandro <alessandro.tacini@fieb.edu.br>
2023-08-18 22:52:58 -0300

    Add método olaMundo()

--- src/main/java/br/com/belval/crud/CrudApplication.java
+++ src/main/java/br/com/belval/crud/CrudApplication.java
@@ -2,12 +2,20 @@ package br.com.belval.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class CrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String olaMundo() {
+		return "Olá Mundo!!!";
 	}
 
 }
