a801029e02766d21835b9d8950cf83053526c863
Alessandro <alessandro.tacini@fieb.edu.br>
2023-09-22 20:54:16 -0300

    Add metodos list() e detalhe()

--- src/main/java/br/com/belval/crud/crontroller/ProdutoController.java
+++ src/main/java/br/com/belval/crud/crontroller/ProdutoController.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -33,4 +35,32 @@ public class ProdutoController {
 		return modelAndView;
 	}
 
+	@GetMapping("/produto/list")
+	public String list(Model model) {
+		model.addAttribute("produtos", lista);
+		return "lista-produtos";
+	}
+	
+	@GetMapping("/produto/{id}")
+	public String detalhe(@PathVariable int id, Model model) {
+		Produto encontrou = null;
+		for(Produto p : lista) {
+			if (p.getId() == id) {
+				//encontrou o produto solicitado
+				encontrou = p;
+				break;
+			}
+		}
+		
+		if (encontrou != null) {
+			model.addAttribute("novoProduto", encontrou);
+			return "novo-produto-criado";
+		}
+		
+		return "produto-nao-encontrado";
+	}
+	
+	
+	
+	
 }
