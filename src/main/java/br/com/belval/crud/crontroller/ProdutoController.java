858073b40693b80fe0f9438668153ca32882fef5
Alessandro <alessandro.tacini@fieb.edu.br>
2023-09-22 21:11:58 -0300

    detalhe foi refatorado

--- src/main/java/br/com/belval/crud/crontroller/ProdutoController.java
+++ src/main/java/br/com/belval/crud/crontroller/ProdutoController.java
@@ -43,6 +43,17 @@ public class ProdutoController {
 	
 	@GetMapping("/produto/{id}")
 	public String detalhe(@PathVariable int id, Model model) {
+		Produto produto = buscarPorId(id);
+		
+		if (produto != null) {
+			model.addAttribute("novoProduto", produto);
+			return "novo-produto-criado";
+		}
+		
+		return "produto-nao-encontrado";
+	}
+
+	private Produto buscarPorId(int id) {
 		Produto encontrou = null;
 		for(Produto p : lista) {
 			if (p.getId() == id) {
@@ -51,13 +62,7 @@ public class ProdutoController {
 				break;
 			}
 		}
-		
-		if (encontrou != null) {
-			model.addAttribute("novoProduto", encontrou);
-			return "novo-produto-criado";
-		}
-		
-		return "produto-nao-encontrado";
+		return encontrou;
 	}
 	
 	
